34c8c62771af4299ebcda181ca5cbec7266884b2
ZALFA <110007888+zalfaaaaa@users.noreply.github.com>
2022-11-04 20:39:59 +0700

    Delete moacastle directory

--- moacastle/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$id=$_GET['idmoa'];
-
-$d = new PDO('mysql:host=localhost;dbname=moac','root',''); 
-$query = $d->query("DELETE FROM `moaorder` WHERE `idmoa`='$id' ");
-
-if($query){
-    header('location:homea.php');}
